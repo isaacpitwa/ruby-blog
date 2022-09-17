2f162d992b58c25ae428313f06dc98958f32e834
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-17 10:29:03 +0300

    Add counters to models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  def update_likes_counter
+    post.likes_counter = 0 if post.likes_counter.nil?
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts
   has_many :likes
-  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_three_posts
     posts.order!(created_at: :desc).limit(3)
   end
