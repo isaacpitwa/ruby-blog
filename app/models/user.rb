6218aa13278965500679b4c49f59c519d2a06db5
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-17 11:39:41 +0300

    Add username Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts
   has_many :likes
+  validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_three_posts
