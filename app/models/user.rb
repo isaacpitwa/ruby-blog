1330a78ddd4a9cd08d1bdcac76af9ec59771a287
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-17 10:24:59 +0300

    Return recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :likes
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  def most_recent_three_posts
+    posts.order!(created_at: :desc).limit(3)
+  end
 end
