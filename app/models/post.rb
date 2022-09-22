110c83feb3314748f12f8d13ca632dbd66ddd588
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-22 22:41:17 +0300

    Add DB Seeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,8 @@ class Post < ActiveRecord::Base
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_counter
-    user.posts_counter = 0 if user.posts_counter.nil?
-    user.increment!(:posts_counter)
+    author.posts_counter = 0 if author.posts_counter.nil?
+    author.increment!(:posts_counter)
   end
 
   def most_recent_five_comments
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = User.create([{ name: "Demo User", photo: "https://i.imgur.com/XyqQZ.jpg", bio: "This is a demo user. Please do not use this user for any purpose." }, { name: "John Doe", photo: "https://i.imgur.com/XyqQZ.jpg", bio: "This is a demo user. Please do not use this user for any purpose." }])
+
+posts = Post.create([{ title: "Demo Post", text: "This is a demo post. Please do not use this post for any purpose.", author_id: users[0].id, user_id: users[0].id }, { title: "Demo Post", text: "This is a demo post. Please do not use this post for any purpose.", author_id: users[0].id, user_id: users[0].id }, { title: "Demo Post", text: "This is a demo post. Please do not use this post for any purpose.", author_id: users[0].id, user_id: users[0].id }])
+posts[0].update_post_counter();
+posts[1].update_post_counter();
+posts[2].update_post_counter();
+
+comments = Comment.create([{ text: "This is a demo comment. Please do not use this comment for any purpose.", post_id: posts[0].id, author_id: users[0].id, user_id: users[0].id }, { text: "This is a demo comment. Please do not use this comment for any purpose.", post_id: posts[0].id, author_id: users[0].id, user_id: users[0].id }, { text: "This is a demo comment. Please do not use this comment for any purpose.", post_id: posts[0].id, author_id: users[0].id, user_id: users[0].id }])
+comments[0].update_comment_counter();
+comments[1].update_comment_counter();
+comments[2].update_comment_counter();
+    
+likes = Like.create([{ post_id: posts[0].id, author_id: users[0].id}, { post_id: posts[0].id, author_id: users[0].id }, { post_id: posts[0].id, author_id: users[0].id}])
+likes[0].update_likes_counter();
+likes[1].update_likes_counter();
+likes[2].update_likes_counter();
+
+puts "Seeded #{users.count} users, #{posts.count} posts, #{comments.count} comments, and #{likes.count} likes."
