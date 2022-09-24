5a0d220eee4a93245134a9e30acdee08d1cdb4cd
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-24 15:41:44 +0300

    Fix Rubocop linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
     @user = current_user
     @post = Post.find(params[:id])
   end
+
   def comment
     @post = Post.find(params[:id])
     p params
